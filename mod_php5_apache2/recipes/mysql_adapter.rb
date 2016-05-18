4381af48c9f8baa2d80dae68522d090f629a1fa9
Philipp Tkachev <zoonman@gmail.com>
2016-05-18 17:19:32 -0400

    Add custom php56-mysqlnd recipe

--- mod_php5_apache2/recipes/mysql_adapter.rb
+++ mod_php5_apache2/recipes/mysql_adapter.rb
@@ -1,7 +1,7 @@
 package 'php-mysql' do
   package_name value_for_platform_family(
-    'rhel' => 'php56-mysql',
-    'debian' => 'php56-mysql'
+    'rhel' => 'php56-mysqlnd',
+    'debian' => 'php56-mysqlnd'
   )
   retries 3
   retry_delay 5
